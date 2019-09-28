beb335812d1ca104eb985bb09531717caff544d5
Julia Kim <kimjulia313@gmail.com>
2019-09-28 11:20:53 -0500

    Install Grommet requirements

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,23 @@
 import React, {Component} from 'react';
+import { Box, Button, Grommet } from 'grommet';
 
 class Home extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {};
-    }
-    render() {
-        return (
-            <div>You are on the home page</div>
-        )
-    }
+  constructor(props) {
+    super(props)
+    this.state = {};
+  }
+  
+  render() {
+    return (
+      <Grommet>
+        <Box align="center" background="neutral-2">
+          <Button
+            label="testing, may delete"
+          />
+        </Box>
+      </Grommet>
+    )
+  }
 }
 
 export default Home;
