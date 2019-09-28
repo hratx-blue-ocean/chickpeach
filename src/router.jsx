7511cd20a06f11c0b9c22764ee6a6687cd2d1ae0
Arohan Dutt <arohandutt@gmail.com>
2019-09-28 17:38:11 -0500

    Handle header, hero pic, image, and ingredients

--- src/router.jsx
+++ src/router.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Home from './components/Home.jsx'
 import Template from './components/Template.jsx'
+import RecipeView from './components/RecipeView.jsx'
 
 function RouteManager() {
   return (
@@ -9,6 +10,7 @@ function RouteManager() {
       <div>
         <Route exact path="/" component={Home} />
         <Route exact path="/template" component={Template} />
+        <Route exact path="/recipeView" component={RecipeView} />
       </div>
     </Router>
   );
