f27a04faf039076a5bd24d8f18b8883342493b42
Whitney Lee <whitneyleephotography@gmail.com>
2019-09-28 17:33:49 -0500

    Render to page

--- src/components/ShoppingList.jsx
+++ src/components/ShoppingList.jsx
@@ -1,20 +1,26 @@
 import React from 'react';
-import { Grommet, Box } from "grommet";
+import { Grommet } from 'grommet';
+import GroceryItem from  './GroceryItem.jsx'
 
 
 class ShoppingList extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {};
+    this.state = {
+      ingredients: [['soymilk', '2 cups'], ['bananas', 2], ['flaxseed', '1 tbs']]
+    };
   }
 
   render() {
     return (
-      <Grommet>
-        <div id='shopping_list_container'>
+      
+        <div id='shopping_list'>
           <h1 className='ShoppingList_text'>Shopping List</h1>
+          <div className='grocery_item_container'>
+            <GroceryItem ingredients={this.state.ingredients} />
+          </div>   
         </div>
-      </Grommet>
+      
     )
   }
 }
