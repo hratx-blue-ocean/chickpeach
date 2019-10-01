416d5b61f654016f37b0b595ec1e1ece95ee46fb
Julia Kim <kimjulia313@gmail.com>
2019-10-01 01:07:27 -0500

    Implement bottom half of 'Create Recipe' page

--- src/components/CreateRecipe.jsx
+++ src/components/CreateRecipe.jsx
@@ -4,10 +4,49 @@ import { Heading, TextInput, Select, Button, TextArea } from 'grommet';
 import MaterialIcon from 'material-icons-react';
 import NavBar from './NavBar.jsx';
 
+const dummyRecipe = {
+  title: 'Pad Thai',
+  images: ['https://images.unsplash.com/photo-1473093226795-af9932fe5856?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1885&q=80'],
+  ingredients: [
+    '5 ounces pad Thai rice noodles',
+    '3 tablespoons vegetable oil',
+    '1 large egg, room temperature',
+    '6 medium shrimp, peeled, deveined (optional)',
+    '2 tablespoons 1x½x⅛-inch slices pressed tofu (bean curd)',
+    '1 tablespoon sweet preserved shredded radish, rinsed, chopped into 1-inch pieces',
+    '1 cup bean sprouts',
+    '5 tablespoons tamarind water, or 2 tablespoons plus 1 teaspoon tamarind paste mixed with 2 tablespoons plus 1 teaspoon water',
+    '1½ tablespoons (or more) Thai fish sauce (nam pla)',
+    '1½ tablespoons simple syrup, preferably made with palm sugar',
+    '4 garlic chives, 2 cut into 1-inch pieces',
+    '½ teaspoon ground dried Thai chiles, divided',
+    '2 tablespoons crushed roasted, unsalted peanuts, divided',
+    '2 lime wedges',
+  ],
+  directions: [
+    'Place noodles in a large bowl; pour hot water over to cover. Let soak until tender but not mushy, 5–10 minutes. Drain; set aside.',
+    'Heat vegetable oil in a wok or large skillet over medium-high heat. Add egg; stir until barely set, about 30 seconds. Add shrimp, if using. Cook, stirring, until shrimp and egg are almost cooked through, 2–3 minutes. Add tofu and radish; cook for 30 seconds. Add noodles and cook for 1 minute. Stir in sprouts. Add tamarind water, fish sauce, and simple syrup and stir-fry until sauce is absorbed by noodles and noodles are well coated, about 1 minute. Stir in chopped garlic chives. Add 1/4 tsp. ground chiles and 1 Tbsp. peanuts and toss well. Transfer to serving plates.',
+    'Garnish with remaining 1/4 tsp. ground chiles, 1 Tbsp. peanuts, and lime wedges.',
+  ],
+  nutrition_info: {
+    calories: 500,
+    carbs: '37g',
+    sugar: '16g',
+    fat: '39g',
+    sodium: '1073mg',
+    fiber: '9g',
+    net_carbs: '28g',
+    added_sugar: '0g',
+    saturated_fat: '26g',
+    protein: '20g'
+  }
+};
+
 class CreateRecipe extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
+      recipe: dummyRecipe
     };
   }
 
@@ -16,13 +55,13 @@ class CreateRecipe extends React.Component {
       <div>
         <Heading className="header1">Create Recipe</Heading>
         <div className="menu">
-          <p>Title</p>
+          <h4>Title</h4>
           <TextInput
             placeholder="type here"
             // value={value}
             onChange={event => setValue(event.target.value)}
           />
-          <p>Add Ingredients</p>
+          <h4>Add Ingredients</h4>
           <TextInput
             placeholder="type here"
             // value={value}
@@ -44,7 +83,7 @@ class CreateRecipe extends React.Component {
             label
             onClick={() => {}}
           />
-          <p>Add Directions</p>
+          <h4>Add Directions</h4>
           <TextArea
             placeholder="type here"
             // value={value}
@@ -55,6 +94,27 @@ class CreateRecipe extends React.Component {
             label
             onClick={() => {}}
           />
+        </div>
+        <div className={'grey_container'}>
+          <h3>Ingredients</h3>
+          {
+            this.state.recipe.ingredients.map(ingredient => {
+              return <p className={'recipe_ingredient'} key={ingredient}>{ingredient}</p>
+            })
+          }
+        </div>
+        <div id={'recipe_directions_container'}>
+          <h3 className={'recipe_directions_label'}>Directions</h3>
+          {
+            this.state.recipe.directions.map((step, index) => {
+              return (
+                <div className={'recipe_step'} key={index}>
+                  <div className={'recipe_step_number'}>{index + 1 + '.'}</div>
+                  <div>{step}</div>
+                </div>
+              )
+            })
+          }
         </div>
         <NavBar />
       </div>
