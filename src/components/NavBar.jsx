35748d1d110431b54242ca58e39e43ff6ef1531f
Julia Kim <kimjulia313@gmail.com>
2019-10-02 10:59:07 -0500

    Update routes

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,8 +5,8 @@ import MaterialIcon from 'material-icons-react';
 const NavBar = () => {
   return (
     <div id='navBar'>
-      <Link to='/'><MaterialIcon icon="search" color='whitesmoke'size={38} /></Link>
-      <Link to='/'><MaterialIcon icon="restaurant" color='whitesmoke'size={38} /></Link>
+      <Link to='/recipeSearch'><MaterialIcon icon="search" color='whitesmoke'size={38} /></Link>
+      <Link to='/menu'><MaterialIcon icon="restaurant" color='whitesmoke'size={38} /></Link>
       <Link to='/shoppinglist'><MaterialIcon icon="assignment" color='whitesmoke'size={38} /></Link>
     </div>
   )
