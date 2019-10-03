9ac0bfaaf3c964ab625b5bbb753422771eb30207
Whitney Lee <whitneyleephotography@gmail.com>
2019-10-03 10:11:51 -0500

    Consolodates same ingredients from different recipes

--- src/components/ShoppingList.jsx
+++ src/components/ShoppingList.jsx
@@ -8,19 +8,61 @@ import NavBar from './NavBar.jsx';
 
 const ShoppingList = (props) => {
   const [ingredients, updateIngredients] = useState(props.ingredients)
+
+  const individualIngredientsArray = function(arrOfRecipes) {
+    let allIngredients = []
+    arrOfRecipes.map((recipe) => {
+      allIngredients.push(recipe.ingredients);
+    })
+    return allIngredients.flat();
+  }
+
+  const allIngredients = individualIngredientsArray(props.ingredients);
+ 
+  function consolodateIngredients(ingredientsToConsolodate) {
+    const consolodated = ingredientsToConsolodate.reduce((accumulator, ingredient, index) => {
+      for (let val of accumulator) {
+        if (
+          ingredient.name === val.name &&
+          ingredient.unit === val.unit
+        ) {
+          val.quantity = val.quantity + ingredient.quantity;
+          return accumulator;
+        } 
+        // else if (ingredient.name === val.name) {
+        //   maybe do something
+        // } 
+      }
+
+      ingredient.id = index;
+      accumulator.push(ingredient);
+      return accumulator;   
+
+    }, [])
+
+    return consolodated;
+  }
+
+  const ingredientsForList = consolodateIngredients(allIngredients);
+
   return (
     <div>
       <div>
         <div id='shopping_list'>
           <h1 className='ShoppingList_text'>Shopping List</h1>
           <div className='grocery_line_top'>
-          {ingredients.map((recipe) => {
-            return (
-            <div key={recipe.name} className='grocery_item_container'>
-              <GroceryItem recipe={recipe} />
+            <div className='grocery_item_container'>
+            <div> 
+              {ingredientsForList.map((ingredient) => {
+                return (
+                  <div key={ingredient.id} className='grocery_line'>
+                    <span className='list_item list_item_left'>{ingredient.name}</span>
+                    <span className='list_item list_item_right'>{`${ingredient.quantity} ${ingredient.unit}`}</span>
+                  </div>
+                )
+              })} 
+            </div>
             </div>   
-            )
-          })}
           </div>
         </div>
       </div>
@@ -29,6 +71,7 @@ const ShoppingList = (props) => {
       </div>
     </div>
   )
+
 }
 
 export default ShoppingList;
