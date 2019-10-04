d600d49b205760b6a5dacd63d9a17ba7d510ceaa
Julia Kim <kimjulia313@gmail.com>
2019-10-03 20:53:16 -0500

    Update buttons on menu page

--- src/components/MenuCard.jsx
+++ src/components/MenuCard.jsx
@@ -1,9 +1,54 @@
 import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { updateMenu } from './actions';
 import { withRouter } from "react-router-dom";
+import axios from 'axios';
 import { Button } from 'grommet';
 
 const MenuCard = (props) => {
-  const onClick = (id) => {
+  const dispatch = useDispatch();
+  const { view } = useSelector(state => state.Menu);
+
+  const getMenu = () => {
+    axios.get('/menuitems', {
+        params: {
+          id: 'a123'
+          // preferences.uid
+        }
+      })
+      .then(({ data }) => {
+        dispatch(updateMenu(data));
+      })
+      .catch(error => console.log(error));
+  };
+
+  const getFavorites = () => {
+    axios.get('/favoriteitems', {
+        params: {
+          id: 'a123'
+          // preferences.uid
+        }
+      })
+      .then(({ data }) => {
+        dispatch(updateMenu(data));
+      })
+      .catch(error => console.log(error));
+  };
+
+  const getHistory = () => {
+    axios.get('/saveditems', {
+        params: {
+          id: 'a123'
+          // preferences.uid
+        }
+      })
+      .then(({ data }) => {
+        dispatch(updateMenu(data));
+      })
+      .catch(error => console.log(error));
+  };
+
+  const onCookClick = (id) => {
     return (
       // <Redirect to={{
       //   pathname: '/recipeView',
@@ -17,6 +62,38 @@ const MenuCard = (props) => {
       })
     )
   };
+
+  const onRemoveClick = () => {
+    if (view === 'Menu') {
+      axios.put('/removemenuitem', {
+        user_id: 'a123', // preferences.uid <- Replace once there are more users in database
+        recipe_id: props.recipe.id
+      })
+      .then(alert('Successfully removed recipe from menu'))
+      .then(getMenu())
+      .catch(error => console.log(error));
+    }
+
+    if (view === 'Favorites') {
+      axios.put('/removefromfavorites', {
+        user_id: 'a123', // preferences.uid <- Replace once there are more users in database
+        recipe_id: props.recipe.id
+      })
+      .then(alert('Successfully removed recipe from favorites'))
+      .then(getFavorites())
+      .catch(error => console.log(error));
+    }
+
+    if (view === 'History') {
+      axios.put('/removefromhistory', {
+        user_id: 'a123', // preferences.uid <- Replace once there are more users in database
+        recipe_id: props.recipe.id
+      })
+      .then(alert('Recipe removed from history'))
+      .then(getHistory())
+      .catch(error => console.log(error));
+    }
+  }
 
   return (
     <div className="card">
@@ -33,8 +110,8 @@ const MenuCard = (props) => {
           </p>
         </div>
         <div className="card_footer menu_footer">
-          <Button className={'primary_button'} onClick={() => {onClick(props.recipe.id)}}>Preview</Button>
-          <Button className={'primary_button'} onClick={() => {}}>Cook</Button>
+          <Button className={'primary_button'} onClick={() => {onCookClick(props.recipe.id)}}>Cook</Button>
+          <Button className={'primary_button'} onClick={onRemoveClick}>Remove</Button>
         </div>
       </div>
     </div>
