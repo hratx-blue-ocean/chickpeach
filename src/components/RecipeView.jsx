9954d2d391ad0882439b29dba10278476008c878
Julia Kim <kimjulia313@gmail.com>
2019-10-05 13:32:56 -0500

    Replace dummy user with actual users

--- src/components/RecipeView.jsx
+++ src/components/RecipeView.jsx
@@ -49,7 +49,7 @@ const RecipeView = (props) => {
   // Menu -> Recipe View
   const addToFavorites = () => {
     axios.put('/addtofavorites', {
-      user_id: 'a123', // preferences.uid <- Replace once there are more users in database
+      user_id: preferences.uid,
       recipe_id: props.history.location.state.id
     })
     .then (({ data }) => {
@@ -66,7 +66,7 @@ const RecipeView = (props) => {
 
   const removeFromMenu = () => {
     axios.put('/removemenuitem', {
-      user_id: 'a123', // preferences.uid <- Replace once there are more users in database
+      user_id: preferences.uid,
       recipe_id: props.history.location.state.id
     })
     .then(dispatch(updateServings(servings - props.history.location.state.servings)))
@@ -81,7 +81,7 @@ const RecipeView = (props) => {
   // Favorites -> Recipe View
   const addToMenu = () => {
     axios.put('/addtomenu', {
-      user_id: 'a123', // preferences.uid <- Replace once there are more users in database
+      user_id: preferences.uid,
       recipe_id: props.history.location.state.id
     })
     .then(({ data }) => {
@@ -98,7 +98,7 @@ const RecipeView = (props) => {
 
   const removeFromFavorites = () => {
     axios.put('/removefromfavorites', {
-      user_id: 'a123', // preferences.uid <- Replace once there are more users in database
+      user_id: preferences.uid,
       recipe_id: props.history.location.state.id
     })
     .then(dispatch(updateView('Favorites')))
@@ -115,18 +115,18 @@ const RecipeView = (props) => {
     axios.post('/addrecipe', {
       params: {
         action: 'menu',
-        user: 'a123' // preferences.uid
+        user: preferences.uid
       },
       data: recipe
     })
-      .then(alert('Successfully added recipe to menu'))
+      .then(alert('Successfully added recipe to favorites'))
       .catch(error => console.log(error));
   };
 
   // History -> Recipe View
   const removeFromHistory = () => {
     axios.put('/removefromhistory', {
-      user_id: 'a123', // preferences.uid <- Replace once there are more users in database
+      user_id: preferences.uid,
       recipe_id: props.history.location.state.id
     })
     .then(dispatch(updateView('History')))
