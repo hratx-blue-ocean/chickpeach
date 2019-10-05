8cca1241c5ab15cebee1547103c53f961470cb78
Whitney Lee <whitneyleephotography@gmail.com>
2019-10-05 17:21:52 -0500

    Resolve merge conflicts

--- spoonAPI.config.js
+++ spoonAPI.config.js
@@ -1,7 +1,3 @@
 module.exports = {
-<<<<<<< HEAD
   spoonAPIKey: "1f07a2ccbcmsh7e25e1fccb05730p19cefcjsna03f7e6acc89"
-=======
-  spoonAPIKey: process.env.SPOON_API_KEY
->>>>>>> 3b149ed97f983e37aaefbdf6af9405a2aedc9fa1
 }
